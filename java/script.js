49d833b401b0e1405295294a7b055ccf0afe2d00
Brett Sheriff <Sheriff222@gmail.com>
2022-08-11 13:53:08 -0700

    Add the addBookToLibrary function

--- java/script.js
+++ java/script.js
@@ -1,18 +1,32 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        let readStatus = "";
-        if (read) {
-            readStatus = "read"    
-        } else {
-            readStatus = "not read"
-        };``
-        console.log(title + " by " + author + ", " + pages + " pages, " + readStatus);
-    }
+}
+
+Book.prototype.info = function() {
+    let readStatus = "";
+    if (this.read) {
+        readStatus = "read"    
+    } else {
+        readStatus = "not read"
+    };
+    console.log(this.title + " by " + this.author + ", " + this.pages + " pages, " + readStatus);
+}
+
+//Add a new book to my Library based off user inputs
+function addBookToLibrary() {
+    let newBookTitle = prompt("Title", "The Hobbit");
+    let newBookAuthor = prompt("Author", "J.R.R. Tolkien");
+    let newBookPages = prompt("Pages", 295);
+    let newBookRead = prompt("Read?", false);
+    myLibrary.push(new Book(newBookTitle, newBookAuthor, newBookPages, newBookRead));
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-theHobbit.info();
+const theWayOfKings = new Book("The Way of Kings", "Brandon Sanderson", 1001, true);
+addBookToLibrary();
+
